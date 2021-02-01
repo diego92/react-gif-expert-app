e67cdbce503ddeb0de27fcb20b37059b52da0d30
Diego Gonzalez <diego_bosteroxeneize@hotmail.com>
2021-02-01 20:06:00 -0300

    Aplicando useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export const GifGrid = ({ category }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getGifs();
+  }, []); // no poner dependecias aca hace que no se ejecute de nuevo la peticion
+
   const getGifs = async () => {
     const url =
       "https://api.giphy.com/v1/gifs/search?q=dragon+ball&limit=10&api_key=d1DvYQ5f6O59f814O2FX5N3Gca3dBXwX";
@@ -16,8 +22,13 @@ export const GifGrid = ({ category }) => {
     console.log(gifs);
   };
 
-  getGifs();
-  return <h3>{category}</h3>;
+  console.log(count);
+  return (
+    <div>
+      <h3>{category}</h3>
+      <button onClick={() => setCount(count + 1)}>Buscar</button>
+    </div>
+  );
 };
 
 GifGrid.propTypes = {
