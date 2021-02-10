3ce9704479e89ee02f934e85fdd295cb34af0372
Diego Gonzalez <diego_bosteroxeneize@hotmail.com>
2021-02-09 23:57:16 -0300

    Pruebas en el componente GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -14,4 +14,22 @@ describe("pruebas en <GifGridItem />", () => {
   test("debe mostrar <GifGridItem /> correctamente", () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  test("debe de tener un parrafo con el title", () => {
+    const p = wrapper.find("p");
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test("debe de tener la imagen igual al url y alt de los props", () => {
+    const img = wrapper.find("img");
+    // console.log(img.props().src);
+    // console.log(img.prop("src")); de estas maneras puedo acceder a las propiedades de un elemento
+    expect(img.prop("src")).toBe(url);
+    expect(img.prop("alt")).toBe(title);
+  });
+
+  test("debe de tener la clase animate__fadeIn", () => {
+    const div = wrapper.find("div");
+    expect(div.prop("className").includes("animate__fadeIn")).toBe(true); // valida que tenga la clase de css
+  });
 });
