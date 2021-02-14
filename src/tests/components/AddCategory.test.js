bb6b21a585f6e25cf07d3ec4c36396a7ebcefcd6
Diego Gonzalez <diego_bosteroxeneize@hotmail.com>
2021-02-14 12:47:33 -0300

    Simular cambios en un input

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -5,9 +5,17 @@ import { AddCategory } from "../../components/AddCategory";
 
 describe("pruebas en el componente <AddCategory />", () => {
   const setCategories = () => {};
-  test("debe de mostrarse correctamente ", () => {
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
+  test("debe de mostrarse correctamente ", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("debe de cambiar la caja de texto", () => {
+    const input = wrapper.find("input");
+    const value = "Hola Mundo";
+    input.simulate("change", { target: { value } });
+
+    expect(wrapper.find("p").text()).toBe("Hola Mundo");
   });
 });
