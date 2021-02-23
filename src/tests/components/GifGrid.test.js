1a40c7f1a75467b6c7aadf70185431fb07745ead
Diego Gonzalez <diego.gonzalez@hasar.com>
2021-02-23 10:50:51 -0300

    Pruebas en componente GifGrid

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+import "@testing-library/jest-dom";
+import { GifGrid } from "../../components/GifGrid";
+import { useFetchGifs } from "../../hooks/useFetchGifs";
+jest.mock("../../hooks/useFetchGifs");
+
+describe("Pruebas en <GifGrid />", () => {
+  const category = "One Punch";
+
+  //   let wrapper = shallow(<GifGrid category={category} />); // no es buena practica pero sino no aplica las ayudas
+
+  //   beforeEach(() => {});
+
+  test("debe mostrar <GifGrid /> correctamente", () => {
+    useFetchGifs.mockReturnValue({ data: [], loading: true });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("debe de mostrar items cuando se cargan imagenes useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "abc",
+        url: "http://localhost:4000/cualquierimagen.jpg",
+        title: "Cualquier cosa",
+      },
+    ];
+    useFetchGifs.mockReturnValue({ data: gifs, loading: false });
+
+    const wrapper = shallow(<GifGrid category={category} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
